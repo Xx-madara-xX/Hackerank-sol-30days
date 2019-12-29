cbbb3d0afbbd66ef08b783d1ca5c8d76234557dc
Divyansh Verma <59335379+Xx-madara-xX@users.noreply.github.com>
2019-12-29 18:55:40 +0530

    Create Day3.py
    Day 3 : Intro to Conditional Statements

---
+++ Day3.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input())
+
+    
+if N in range(1,100,2):
+    print ("Weird")
+elif N in range(2,6,2):
+    print ("Not Weird")
+elif N in range(6,21,2):
+    print ("Weird")
+elif N in range(20,101,2):
+    print ("Not Weird")
